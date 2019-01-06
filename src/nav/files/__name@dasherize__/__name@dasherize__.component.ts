0858cf1456cd9dbdd9216de36311acc4821e7f8d
Dhiman Talapatra <dhiman.talapatra@gmil.com>
2019-01-06 23:41:09 +0530

    added i18n changs in template. assuming translation is configred with ngx translate

--- src/nav/files/__name@dasherize__/__name@dasherize__.component.ts
+++ src/nav/files/__name@dasherize__/__name@dasherize__.component.ts
@@ -1,10 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-
+<% if(i18n){%>
+    import {TranslateService} from '@ngx-translate/core';
+<%}%>
 @Component({
     selector: '<%=dasherize(name)%>',
     templateUrl: './<%=dasherize(name)%>.component.html',
     styleUrls: ['./<%=dasherize(name)%>.component.css']
 })
 export class <%= classify(name) %>Component {
-
+    <% if(i18n){%>
+        constructor(
+            private _translate: TranslateService 
+        ){}
+    
+        setLanguage(lang:string){
+            this._translate.use(lang);
+        }
+    <%}%>
 }
